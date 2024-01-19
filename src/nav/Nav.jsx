721565743d1f6b112ee9eca6be85ce42c85bcc9d
Samayoa <fsamayoal@gmail.com>
2024-01-19 08:08:35 -0600

    barras de navegacion

--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -4,11 +4,18 @@ import styles from "../nav/nav.module.css"
 const Nav = () => {
     const location = useLocation();
   return (
+    <div>
+        <div className={styles.logoContainer}>
+        <Link to="/home" ><img className={styles.image} src="https://i.imgur.com/e3YODr9.png"></img></Link>
+        <Link to="/LigaMayorOnefa" className={`${styles.navigation} ${location.pathname === '/LigaMayorOnefa' ? styles.selected : ''}`}>Liga Mayor Onefa</Link>
+        <Link to="/IntermediaOnefa" className={`${styles.navigation} ${location.pathname === '/IntermediaOnefa' ? styles.selected : ''}`}>Intermedia Onefa</Link>
+        <Link to="/lfa" className={`${styles.navigation} ${location.pathname === '/lfa' ? styles.selected : ''}`}>LFA</Link>
+        <Link to="/ifa" className={`${styles.navigation} ${location.pathname === '/ifa' ? styles.selected : ''}`}>IFA</Link>
+        <Link to="/nfl" className={`${styles.navigation} ${location.pathname === '/nfl' ? styles.selected : ''}`}>NFL</Link>
+        </div>
+
     <div className={styles.navContainer}>
     <div className={styles.linksContainer}>
-        <div className={styles.logoContainer}>
-        <Link to="/home" ><img className="image" src="https://i.imgur.com/e3YODr9.png"></img></Link>
-        </div>
         <Link to="/resultados" className={`${styles.navigation} ${location.pathname === '/resultados' ? styles.selected : ''}`}>Resultados</Link>
         <Link to="/calendario" className={`${styles.navigation} ${location.pathname === '/calendario' ? styles.selected : ''}`}>Calendario</Link>
         <Link to="/posiciones" className={`${styles.navigation} ${location.pathname === '/posiciones' ? styles.selected : ''}`}>Posiciones</Link>
@@ -21,6 +28,7 @@ const Nav = () => {
         <Link to="/tienda" className={`${styles.navigation} ${location.pathname === '/tienda' ? styles.selected : ''}`}>Tienda</Link>
         <Link to="/aprende" className={`${styles.navigation} ${location.pathname === '/aprende' ? styles.selected : ''}`}>Aprende de FBA</Link>
      
+    </div>
     </div>
     </div>
   );
